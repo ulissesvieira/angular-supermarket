9e4171de36c14d50b45b1176ac95b2f21d8f2199
Ulisses Vieira <sr.jvieira03@gmail.com>
2018-01-09 22:43:40 -0300

    Criação da infra para listar os produtos

---
+++ src/app/product/product-mock.class.ts
@@ -0,0 +1,8 @@
+import { Product } from './product.class';
+
+export const PRODUCTS : Product[] = [
+   {id : 1, sku : 'ABF123', name : 'Café XF350', descripton : 'Café Extra Forte Princesa 350g'},
+   {id : 2, sku : 'GTH423', name : 'Chá VD350', descripton : 'Chá Verde Pilão Princesa 350g'},
+   {id : 3, sku : 'LOB526', name : 'Capuccino 500', descripton : 'Capuccino Cheiroso 500g'},
+   {id : 4, sku : 'ZAS097', name : 'Soda 600', descripton : 'Refrigerante Trolly 600ml'},
+];
